3f38aead787c325f8604a7c71519fb656c7a6812
andyto1234 <toshuho@gmail.com>
2022-11-02 14:58:55 +0000

    ebtel multicore update

--- functions_ebtel.py
+++ functions_ebtel.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 
     with mp.Pool(50) as p:
         fline_partial = partial(fline_multi, length, flines, aia_submap, file_list_multi)
-        results = list(tqdm(p.imap(fline_partial, range(len(flines))),total = len(flines)))
+        results = p.map(fline_partial, range(len(flines)))
     for result in results:
         blank_data[result[1], result[0]] += result[2]
 
