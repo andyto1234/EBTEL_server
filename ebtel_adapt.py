35a968d0415d28b3ac067e9f433a2362ac3a4b67
andyto1234 <toshuho@gmail.com>
2022-11-08 15:38:40 +0000

    ebtel adapt map update

--- ebtel_adapt.py
+++ ebtel_adapt.py
@@ -2,7 +2,6 @@ import pfsspy
 import sunpy
 import numpy as np
 from functions_pickle import *
-from functions_data import *
 
 def adapt2pfsspy(filepath, #must already exist on your computer
                  rss=2.5, # Source surface height
@@ -25,11 +24,9 @@ def adapt2pfsspy(filepath, #must already exist on your computer
     # realization is selected
     elif isinstance(realization,int) : adapt_map = adaptMapSequence[realization]
     else : raise ValueError("realization should either be 'mean' or type int ") 
-    save('20110415/bug_adapt_map',adapt_map)
     # pfsspy requires that the y-axis be in sin(degrees) not degrees
     # pfsspy.utils.car_to_cea does this conversion
     adapt_map_strumfric = pfsspy.utils.car_to_cea(adapt_map)
-    save('20110415/bug_adapt_map_strumfric',adapt_map_strumfric)
     # Option to return the magnetogram
     if return_magnetogram : 
         return adapt_map_strumfric
@@ -41,87 +38,3 @@ def adapt2pfsspy(filepath, #must already exist on your computer
         peri_output = pfsspy.pfss(peri_input)
         return peri_output
 
-def adapt2pfsspy(filepath, #must already exist on your computer
-                 rss=2.5, # Source surface height
-                 nr=60, # number of radial gridpoiints for model
-                 realization="mean", #which slice of the adapt ensemble to choose
-                 return_magnetogram = False # switch to true for function to return the input magnetogram
-                ):
-
-    # Load the FITS file into memory
-    # ADAPT includes 12 "realizations" - model ensembles
-    # pfsspy.utils.load_adapt is a specific function that knows
-    # how to handel adapt maps
-    adaptMapSequence = pfsspy.utils.load_adapt(filepath)
-    # If realization = mean, just average them all together
-    if realization == "mean" : 
-        br_adapt_ = np.mean([m.data for m in adaptMapSequence],axis=0)
-        adapt_map = sunpy.map.Map(br_adapt_,adaptMapSequence[0].meta)
-        
-    # If you enter an integer between 0 and 11, the corresponding
-    # realization is selected
-    elif isinstance(realization,int) : adapt_map = adaptMapSequence[realization]
-    else : raise ValueError("realization should either be 'mean' or type int ") 
-    save('20110415/bug_adapt_map',adapt_map)
-    # pfsspy requires that the y-axis be in sin(degrees) not degrees
-    # pfsspy.utils.car_to_cea does this conversion
-    adapt_map_strumfric = pfsspy.utils.car_to_cea(adapt_map)
-    save('20110415/bug_adapt_map_strumfric',adapt_map_strumfric)
-    # Option to return the magnetogram
-    if return_magnetogram : 
-        return adapt_map_strumfric
-    # Otherwise run the PFSS Model and return
-    else :
-        adapt_map_input = sunpy.map.Map(adapt_map_strumfric.data,
-                                        adapt_map_strumfric.meta)
-        peri_input = pfsspy.Input(adapt_map_input, nr, rss)
-        peri_output = pfsspy.pfss(peri_input)
-        return peri_output
-def adapt2pfsspy(filepath, #must already exist on your computer
-                 rss=2.5, # Source surface height
-                 nr=60, # number of radial gridpoiints for model
-                 realization="mean", #which slice of the adapt ensemble to choose
-                 return_magnetogram = False # switch to true for function to return the input magnetogram
-                ):
-
-    # Load the FITS file into memory
-    # ADAPT includes 12 "realizations" - model ensembles
-    # pfsspy.utils.load_adapt is a specific function that knows
-    # how to handel adapt maps
-    adaptMapSequence = pfsspy.utils.load_adapt(filepath)
-    # If realization = mean, just average them all together
-    if realization == "mean" : 
-        br_adapt_ = np.mean([m.data for m in adaptMapSequence],axis=0)
-        adapt_map = sunpy.map.Map(br_adapt_,adaptMapSequence[0].meta)
-        
-    # If you enter an integer between 0 and 11, the corresponding
-    # realization is selected
-    elif isinstance(realization,int) : adapt_map = adaptMapSequence[realization]
-    else : raise ValueError("realization should either be 'mean' or type int ") 
-    save('20110415/bug_adapt_map',adapt_map)
-    # pfsspy requires that the y-axis be in sin(degrees) not degrees
-    # pfsspy.utils.car_to_cea does this conversion
-    adapt_map_strumfric = pfsspy.utils.car_to_cea(adapt_map)
-    save('20110415/bug_adapt_map_strumfric',adapt_map_strumfric)
-    # Option to return the magnetogram
-    if return_magnetogram : 
-        return adapt_map_strumfric
-    # Otherwise run the PFSS Model and return
-    else :
-        adapt_map_input = sunpy.map.Map(adapt_map_strumfric.data,
-                                        adapt_map_strumfric.meta)
-        peri_input = pfsspy.Input(adapt_map_input, nr, rss)
-        peri_output = pfsspy.pfss(peri_input)
-        return peri_output
-
-def hmi2pfsspy(hmi_map, #must already exist on your computer
-                 rss=2.5, # Source surface height
-                 nr=60, # number of radial gridpoiints for model
-                 realization="mean", #which slice of the adapt ensemble to choose
-                 return_magnetogram = False # switch to true for function to return the input magnetogram
-                ):
-
-    hmi_map_cea = hmi_to_cea(hmi_map)    # Option to return the magnetogram
-    pfss_input = pfsspy.Input(hmi_map_cea, nr, rss)
-    peri_output = pfsspy.pfss(pfss_input)
-    return peri_output
