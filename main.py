b2a44ecd2b08b60151607ad984c03cb2201a75b1
andyto1234 <toshuho@gmail.com>
2023-02-01 20:52:57 +0900

    ebtel pfss parameters with closed and opened field

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ ar_width = 600*u.arcsec
 ar_height = 600*u.arcsec
 
 # Resample PFSS
-m_hmi_resample = m_hmi.resample((2700, 1620)*u.pix)
+m_hmi_resample = m_hmi.resample((1800, 1080)*u.pix)
 
 # Setting parameters for PFSSpy
 print('Setting parameters for PFSSpy')
